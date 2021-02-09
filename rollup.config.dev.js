70f0e6122514a17429ff986af99ea10b3b51f7d1
ailon <ailon@ailon.org>
2021-02-09 11:37:14 +0200

    add manual testing page structure and assets

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -3,6 +3,7 @@ import htmlTemplate from 'rollup-plugin-generate-html-template';
 import dev from 'rollup-plugin-dev';
 import livereload from 'rollup-plugin-livereload';
 import pkg from './package.json';
+import copy from 'rollup-plugin-copy';
 import svgo from 'rollup-plugin-svgo';
 
 export default {
@@ -19,6 +20,11 @@ export default {
     htmlTemplate({
       template: 'test/manual/template.html',
       target: 'index.html'      
+    }),
+    copy({
+      targets: [{
+        src: 'test/manual/images/**/*', dest: 'build-dev/images'
+      }]
     }),
     dev('build-dev'),
     livereload('build-dev')
