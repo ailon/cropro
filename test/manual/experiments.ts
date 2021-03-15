0d8c9ee3af82f1ff68c7b139f0942a689b16074d
Alan Mendelevich <ailon@ailon.org>
2021-03-15 11:10:40 +0200

    add more manual testing buttons

--- test/manual/experiments.ts
+++ test/manual/experiments.ts
@@ -2,7 +2,7 @@ import { CropArea } from '../../src';
 import { CropAreaState } from '../../src/CropAreaState';
 
 export class Experiments {
-  public showCropArea(target: HTMLImageElement): void {
+  public showCropArea1(target: HTMLImageElement): void {
     const ca = new CropArea(target);
     // ca.gridLines = 8;
     // ca.isGridVisible = false;
@@ -21,12 +21,62 @@ export class Experiments {
     })
     ca.show();
   }
+
+  public showCropArea2(target: HTMLImageElement): void {
+    const ca = new CropArea(target);
+    // ca.gridLines = 8;
+    // ca.isGridVisible = false;
+    // ca.displayMode = 'popup';
+
+    // ca.renderAtNaturalSize = true;
+    // ca.renderWidth = 640;
+    // ca.renderHeight = 480;
+    ca.renderImageType = 'image/jpeg';
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ca.addRenderEventListener((dataUrl: string, state: CropAreaState) => {
+      const res = document.createElement('img');
+      res.src = dataUrl;
+      document.body.appendChild(res);
+    })
+    ca.show();
+  }
+
+  public showCropArea3(target: HTMLImageElement): void {
+    const ca = new CropArea(target);
+    ca.gridLines = 8;
+    // ca.isGridVisible = false;
+    // ca.displayMode = 'popup';
+
+    ca.renderAtNaturalSize = true;
+    // ca.renderWidth = 640;
+    // ca.renderHeight = 480;
+    // ca.renderImageType = 'image/jpeg';
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ca.addRenderEventListener((dataUrl: string, state: CropAreaState) => {
+      const res = document.createElement('img');
+      res.src = dataUrl;
+      document.body.appendChild(res);
+    })
+    ca.show();
+  }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  const targetImg = document.getElementById('testImage1') as HTMLImageElement;
   const experiments = new Experiments();
+
+  const targetImg = document.getElementById('testImage1') as HTMLImageElement;
   targetImg.addEventListener('click', () =>
-    experiments.showCropArea(targetImg)
+    experiments.showCropArea1(targetImg)
   );
+  document.getElementById('openButton1').addEventListener('click', () => {
+    experiments.showCropArea1(targetImg);
+  });
+  document.getElementById('openButton2').addEventListener('click', () => {
+    experiments.showCropArea2(targetImg);
+  });
+  document.getElementById('openButton3').addEventListener('click', () => {
+    experiments.showCropArea3(targetImg);
+  });
 });
